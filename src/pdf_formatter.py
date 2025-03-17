b2868d709e746b5706a2379ab0351fbb30703fdb
ImQQiaoO <1727344097@qq.com>
2025-03-17 17:22:54 +0800

    feat: pdf内容过长时应用换行而不是截断

--- src/pdf_formatter.py
+++ src/pdf_formatter.py
@@ -12,8 +12,8 @@ class PDF(FPDF):
         self.set_font('MSYH', '', 12)
         self.set_y(-15)
         self.cell(0, 10, f"{self.page_no()}", 0, 0, "C")
-        
-        
+
+
 def select_pdf_direction():
     print("请输出导出PDF时的页面方向（输入数字即可，仅支持单选）：")
     print("选择横向时，会展示较为完整的单词释义")
@@ -30,23 +30,24 @@ def select_pdf_direction():
     return direction_choice
 
 
-def truncate_text(pdf, text, max_width):
+def warp_text(pdf, text, max_width) -> tuple:
+    lines_takeup = 1
     text_width = pdf.get_string_width(text)
     margin = 2
     if text_width <= max_width - margin:
-        return text
-    else:
-        ellipsis = "..."
-        ellipsis_width = pdf.get_string_width(ellipsis)
-        available_width = max_width - margin - ellipsis_width
-
-        truncated = ""
-        for char in text:
-            if pdf.get_string_width(truncated + char) <= available_width:
-                truncated += char
-            else:
-                break
-        return truncated + ellipsis
+        return lines_takeup, text
+    warped_text = ""
+    each_line = ""
+    for char in text:
+        if pdf.get_string_width(each_line + char) < max_width - margin:
+            each_line += char
+        else:
+            each_line += '\n'
+            warped_text += each_line
+            lines_takeup +=1
+            each_line = char
+    warped_text += each_line
+    return lines_takeup, warped_text
 
 
 def save_as_pdf(all_words, order_choice):
@@ -73,11 +74,14 @@ def save_as_pdf(all_words, order_choice):
     for idx, (word, interpret) in enumerate(all_words.items()):
         pdf.set_x(start_x)
         fill = idx % 2 == 0
-        pdf.cell(col_widths[0], line_height, str(idx + 1), border=1, align="C", fill=fill)
-        pdf.cell(col_widths[1], line_height, word, border=1, align="C", fill=fill)
-        truncated_interpret = truncate_text(pdf, interpret, col_widths[2])
-        pdf.cell(col_widths[2], line_height, truncated_interpret, border=1, align="C", fill=fill)
-        pdf.ln(line_height)
+        lines_takeup, warped_interpret = warp_text(pdf, interpret, col_widths[2])
+        pdf.cell(col_widths[0], line_height * lines_takeup, str(idx + 1), border=1, align="C", fill=fill)
+        pdf.cell(col_widths[1], line_height * lines_takeup, word, border=1, align="C", fill=fill)
+        x = pdf.get_x()
+        y = pdf.get_y()
+        pdf.multi_cell(col_widths[2], line_height, warped_interpret, border=1, align="L", fill=fill)
+        pdf.set_xy(x + col_widths[2], y)
+        pdf.ln(line_height * lines_takeup)
 
     current_date = datetime.now().strftime('%Y_%m_%d')
     file_name = f"words-{current_date}-{order_options_dict[OrderOption(order_choice)]}-{pdf_direction_dict[PDFDirection(direction)]}.pdf"
